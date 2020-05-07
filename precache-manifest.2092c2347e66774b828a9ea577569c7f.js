bcbac7f275dbc2a88c402efb9c0c52913b89758d
Eyakub Sorkar <eyakubsorkar@gmail.com>
2020-05-07 12:49:25 +0600

    Updates

---
+++ precache-manifest.2092c2347e66774b828a9ea577569c7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f61e0733a4b5f75578b6c2002565ed6",
+    "url": "/quiz-app-reactjs/index.html"
+  },
+  {
+    "revision": "5e7aeedd0641f8f76c41",
+    "url": "/quiz-app-reactjs/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "50777a6d6baa485edb07",
+    "url": "/quiz-app-reactjs/static/js/2.a50e23cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz-app-reactjs/static/js/2.a50e23cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e7aeedd0641f8f76c41",
+    "url": "/quiz-app-reactjs/static/js/main.19bc3008.chunk.js"
+  },
+  {
+    "revision": "1d5bee85bca0980c6d5d",
+    "url": "/quiz-app-reactjs/static/js/runtime-main.132924ff.js"
+  }
+]);
