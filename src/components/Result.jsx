29993be13f8b4426d3f6c6c7a4120547772bc8d6
Eyakub Sorkar <eyakubsorkar@gmail.com>
2020-05-07 12:46:39 +0600

    added the result component

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const Result = ({score, playAgain}) => {
+const Result = ({ score, playAgain }) => {
   return (
     <div className="score-board">
       <div className="score">You scored {score} / 5 correct answers!</div>
-      <button className="playBtn" onClick={playAgain}>Play again!</button>
+      <button className="playBtn" onClick={playAgain}>
+        Play again
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Result
+export default Result;
